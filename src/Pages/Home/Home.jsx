2d08cddf323648104931bb638d0ad7e0107d3a2b
abdelbasset4 <benatmani1999@gmail.com>
2023-06-27 23:10:32 +0200

    design first navbar

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@material-tailwind/react"
+import NavBar from "../../Components/Utility/NavBar"
+
+export default function Home() {
+  return (
+    <>
+      <NavBar/>
+      <div>Home</div>
+      <Button>Button</Button>
+    </>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+// eslint-disable-next-line no-undef
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     // eslint-disable-next-line no-undef
+//     require('flowbite/plugin')
+// ]
+// }
+
+// eslint-disable-next-line no-undef
+module.exports = withMT({
+  content: [
+    "./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      green:{
+        50:"#000000"
+      }
+    },
+    screens: {
+      sm: "540px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "720px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "960px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1140px",
+      // => @media (min-width: 1200px) { ... }
+
+      "2xl": "1320px",
+      // => @media (min-width: 1400px) { ... }
+    },
+    extend: {
+     
+    },
+  },
+  plugins: [],
+});
