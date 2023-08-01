7c821a8f104e1e6957ab252f002b43beff2eee44
abdelbasset4 <benatmani1999@gmail.com>
2023-08-01 23:54:50 +0200

    create category

--- src/Components/Category/AllCategory.jsx
+++ src/Components/Category/AllCategory.jsx
@@ -1,27 +1,27 @@
 
 
 import { CategoryCard } from "./CategoryCard";
-import { Spinner,Alert  } from "@material-tailwind/react";
+import { Spinner,Alert } from "@material-tailwind/react";
 
 // eslint-disable-next-line react/prop-types
 export default function AllCategory({data,isLoading}) {
   // eslint-disable-next-line react/prop-types
-  console.log(`this data: ${data.data}`);
-
   return (
     <>
-    <h2 className="mb-6 text-2xl font-extrabold text-gray-900 ms-4 mt-4">All Categories</h2>
+    <h2 className="mb-6 text-2xl font-extrabold text-gray-900 ms-4 mt-4 ms-4">All Categories</h2>
 
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-6 justify-center mb-6">
       {
         isLoading === false ?(
+
           // eslint-disable-next-line react/prop-types
-          data.data ? (
+          data.length > 0 ? (
+            
             // eslint-disable-next-line react/prop-types
-            data.data.map((category)=>{
-              return (<CategoryCard key={category._id} title={category.name} image={category.image}/>)
+            data.map((item, index) => {
+                return (<CategoryCard key={index} title={item.name} img={item.image}  />)
             })
-          ):(<Alert color="blue">There are no data</Alert>)
+        ) : <Alert color="blue" className="ms-4 w-full">There are no data</Alert>
         ):(<Spinner className="h-16 w-16 text-blue-500/10 flex justify-center items-center" />)
       }
 
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,6 +3,10 @@ import CategorySlice from './Slice/Category/CategorySlice'
 export const store = configureStore({
   reducer: {
     category:CategorySlice
-  }
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    })
   ,devTools:true
 })
