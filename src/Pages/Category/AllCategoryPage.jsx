07e5bbc709be549c0d1188cce06eb3b84aaa02e2
abdelbasset4 <benatmani1999@gmail.com>
2023-08-02 20:35:25 +0200

    complete category section

--- src/Pages/Category/AllCategoryPage.jsx
+++ src/Pages/Category/AllCategoryPage.jsx
@@ -1,26 +1,11 @@
-import { useEffect } from "react";
+
 import AllCategory from "../../Components/Category/AllCategory";
 import NavBar from "../../Components/Utility/NavBar";
 import Pagination from "../../Components/Utility/Pagination";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllCategory } from "../../Redux/Slice/Category/CategoryThunk";
+import useGetAllCategory from "../../hook/category/useGetAllCategory";
 
 export default function AllCategoryPage() {
-  const dispatch = useDispatch();
-  const categories = useSelector(state => state.category.category);
-  const isLoading = useSelector(state => state.category.isLoading);
-  // const erorr = useSelector(state => state.category.erorr);
-  
-  useEffect(() => {
-      dispatch(getAllCategory(`/api/v1/categories?limit=3`));
-  }, [dispatch]);
-    
-    let pageCount = 0;
-    if(categories.paginationResults) {pageCount = categories.paginationResults.numberPages;}
-    
-  const getPageNumber =(page)=>{
-    dispatch(getAllCategory(`/api/v1/categories?limit=3&page=${page}`));
-  }
+  const [categories,isLoading,pageCount,getPageNumber]= useGetAllCategory()
   return (
     <div>
         <NavBar/>
