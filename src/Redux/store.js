eee37c2013e6fd737e05b4d4cd66275f1cee7397
abdelbasset4 <benatmani1999@gmail.com>
2023-08-04 16:07:47 +0100

    finish add sub category

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import CategorySlice from './Slice/Category/CategorySlice'
 import BrandSlice from './Slice/Brand/BrandSlice'
+import SubCategorySlice from './Slice/SubCategory/SubCategorySlice'
 export const store = configureStore({
   reducer: {
     category:CategorySlice,
-    brand:BrandSlice
+    brand:BrandSlice,
+    subCategory:SubCategorySlice
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
