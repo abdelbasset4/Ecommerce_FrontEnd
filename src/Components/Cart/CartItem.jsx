88aa7bcd84578f6a6a7287076030ccfa0ddeac11
abdelbasset4 <benatmani1999@gmail.com>
2023-09-23 14:00:25 +0100

    delete one item and clear all crt

--- src/Components/Cart/CartItem.jsx
+++ src/Components/Cart/CartItem.jsx
@@ -1,44 +1,84 @@
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    Typography,
-  } from "@material-tailwind/react";
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+  Button,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  DialogHeader,
+} from "@material-tailwind/react";
 import Counter from "../Utility/Counter";
-import {AiFillCloseCircle,AiOutlineDelete} from "react-icons/ai"   
+import { AiFillCloseCircle, AiOutlineDelete } from "react-icons/ai";
 import { baseURL } from "../../API/mainBaseURL";
-  export default function CartItem({product}) {
-    // console.log(product.product);
-    return (
+import useDeleteCart from "../../hook/Cart/useDeleteCart";
+import { ToastContainer } from "react-toastify";
+export default function CartItem({ product }) {
+  const [open,handleOpen,handelDeleteCartItem] = useDeleteCart()
+  return (
+    <>
+      <Dialog
+        open={open}
+        handler={handleOpen}
+        animate={{
+          mount: { scale: 1, y: 0 },
+          unmount: { scale: 0.9, y: -100 },
+        }}>
+        <DialogHeader>Delete!</DialogHeader>
+        <DialogBody divider>Do you really want to delete the cart item?</DialogBody>
+        <DialogFooter>
+          <Button
+            variant="text"
+            color="red"
+            onClick={handleOpen}
+            className="mr-1">
+            <span>Cancel</span>
+          </Button>
+          <Button
+            className="bg-gray-900 text-white"
+            onClick={() => handelDeleteCartItem(product._id)}>
+            <span>Confirm</span>
+          </Button>
+        </DialogFooter>
+      </Dialog>
+
       <Card className="flex-col lg:flex-row w-full max-w-[40rem]  mb-4">
-        <CardHeader shadow={false} floated={false} className="w-full lg:w-2/5 shrink-0 m-0 rounded-md relative group cursor-pointer">
-          <img 
+        <CardHeader
+          shadow={false}
+          floated={false}
+          className="w-full lg:w-2/5 shrink-0 m-0 rounded-md relative group cursor-pointer">
+          <img
             src={`${baseURL}/products/${product.product.imageCover}`}
             alt={`${product.product.title}`}
             className="w-full h-full object-cover "
           />
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-30  bg-gray-100">
-                <AiFillCloseCircle size={"2rem"}  color="#333A48"/>
-            </div>
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-30  bg-gray-100">
+            <AiFillCloseCircle size={"2rem"} color="#333A48" />
+          </div>
         </CardHeader>
         <CardBody>
-
           <Typography variant="h4" color="blue-gray" className="mb-2">
-          {`${product.product.title}`}
+            {`${product.product.title}`}
           </Typography>
-          <Typography color="gray" className="font-normal mb-8">
-          {`${product.price}`}$
+          <Typography color="gray" className="font-normal ">
+            {`${product.price}`}$
           </Typography>
+          <div
+            className="w-8 h-8 rounded-full my-4"
+            style={{ backgroundColor: `${product.color}` }}></div>
           <div className="flex justify-between items-center">
-            <Counter quantity={`${product.quantity}`}/>
-            <Typography variant="h5"  className="font-normal text-black">
-                {product.quantity * product.price}$
+            <Counter quantity={`${product.quantity}`} />
+            <Typography variant="h5" className="font-normal text-black">
+              {product.quantity * product.price}$
             </Typography>
           </div>
         </CardBody>
         <div className="absolute right-2 top-2 cursor-pointer">
-            <AiOutlineDelete size={"1.2rem"}/>
+          <AiOutlineDelete onClick={handleOpen} size={"1.2rem"} />
         </div>
       </Card>
-    );
-  }
+      <ToastContainer/>
+    </>
+  );
+}
--- src/Redux/Slice/Cart/CartThunk.js
+++ src/Redux/Slice/Cart/CartThunk.js
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { useInsertData } from "../../../hooks/useInsertData";
 import { useGetDataToken } from "../../../hooks/useGetData";
+import useDeleteData from "../../../hooks/useDeleteData";
 
 export const getUserCart = createAsyncThunk(
   "cart/getAll",
@@ -21,6 +22,33 @@ export const AddToCart = createAsyncThunk(
     const { rejectWithValue } = thunkAPI;
     try {
       const res = await useInsertData(`/api/v1/cart`, data);
+      return res;
+    } catch (err) {
+      return rejectWithValue(err.response);
+    }
+  }
+);
+
+export const clearAllCart = createAsyncThunk(
+  "clear/delete",
+  async (url, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    try {
+      const res = await useDeleteData(url);
+      return res;
+    } catch (err) {
+      return rejectWithValue(err.response);
+    }
+  }
+
+);
+
+export const deleteItemCart = createAsyncThunk(
+  "delete/delete",
+  async (url, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    try {
+      const res = await useDeleteData(url);
       return res;
     } catch (err) {
       return rejectWithValue(err.response);
